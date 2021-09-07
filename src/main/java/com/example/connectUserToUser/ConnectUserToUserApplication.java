a365c95a0a8913a86a0d3bd9784b948cc19b267f
chimaek <pipiru100@gmail.com>
2021-09-07 14:35:57 +0900

    init

---
+++ src/main/java/com/example/connectUserToUser/ConnectUserToUserApplication.java
@@ -0,0 +1,13 @@
+package com.example.connectUserToUser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectUserToUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectUserToUserApplication.class, args);
+	}
+
+}
