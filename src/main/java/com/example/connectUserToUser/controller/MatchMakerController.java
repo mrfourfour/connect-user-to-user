02a7e74c99961a52e2f17e3297dd6f39144efe37
chimaek <pipiru100@gmail.com>
2021-10-01 10:52:55 +0900

    init

--- src/main/java/com/example/connectUserToUser/controller/MatchMakerController.java
+++ src/main/java/com/example/connectUserToUser/controller/MatchMakerController.java
@@ -5,12 +5,8 @@ import com.example.connectUserToUser.service.MatchMakerService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.net.http.HttpResponse;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,13 +14,18 @@ public class MatchMakerController {
     private static final Logger logger = LoggerFactory.getLogger(MatchMakerController.class);
     private final MatchMakerService matchMakerService;
 
-
-    @GetMapping("/api/apply_match")
-    public ResponseEntity<User> apply_match(@RequestParam String userId) throws Exception {
-
-        User user = new User(userId);
-        matchMakerService.operateMatchMaker(user);
-        return ResponseEntity.ok(user);
+    // 리퀘스트 바디는 정확한 dto가 있어야 사용가능 왜? JO로 변환하기 때문
+    @PostMapping("/api/apply_match")
+    public ResponseEntity<String> apply_match(@RequestBody User user) {
+        logger.info("매치 신청");
+        String username = user.getUserId();
+        try {
+            matchMakerService.operateMatchMaker(user);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok(username);
     }
 
 }
